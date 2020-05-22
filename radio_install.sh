6d58b72cf7ca294de9fb261434a14ba065d9c5e8
Sl33pyF0x <firedfox0@protonmail.com>
2020-05-22 15:41:33 -0500

    Added SigDigger Installation

--- radio_install.sh
+++ radio_install.sh
@@ -6,3 +6,10 @@ apt install sox -y
 apt install gqrx-sdr -y
 apt install gnuradio -y
 
+cd Desktop/
+
+wget https://github.com/BatchDrake/SigDigger/releases/download/v0.1.0/SigDigger-0.1.0-x86_64-full.AppImage
+
+chmod +x SigDigger-0.1.0-x86_64-full.AppImage
+
+
